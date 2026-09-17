d6ec3f326ad950e552f7b3d295b03c114ad12cfe
RaviAshray15 <raviashray15@gmail.com>
2026-09-17 11:46:10 +0530

    New Commit

--- Caeser Cipher/day8.py
+++ Caeser Cipher/day8.py
@@ -1,7 +1,7 @@
-def greet():
-    print("Hello world!!")
-    print("Hello Ashray!!")
-    print("What are you doing??")
+def greet_with(name, location):
+    print(f"Hello {name}!!")
+    print(f"You are from {location}")
     
     
-greet()
+greet_with("Ashray", "Hyderabad")
+greet_with("Ravi", "Bangalore")
