93b64bec75ab8886560a46ea68c14690918b688b
RaviAshray15 <raviashray15@gmail.com>
2026-09-26 14:10:22 +0530

    Calculator

--- Calculator/calculator.py
+++ Calculator/calculator.py
@@ -16,3 +16,28 @@ operations = {
     '*': multiply,
     '/': divide,    
 }
+
+def calculator():
+    should_accumulate = True
+    num1 = float(input("Whats your first number?: "))
+
+    while should_accumulate:
+        for i in operations:
+            print(i)
+        symbol = input("Pick an operation: ")
+
+        num2 = float(input("Whats your second number?: "))
+
+        answer = operations[symbol](num1, num2)
+        print(f"{num1} {symbol} {num2} = {answer}")
+
+        choice = input(f"Type 'y' to continue calculating with {answer}, or type 'n' to start over: ")
+
+        if choice == 'y':
+            num1 = answer
+
+        elif choice == 'n':
+            should_accumulate = False
+            calculator()
+            
+calculator()
